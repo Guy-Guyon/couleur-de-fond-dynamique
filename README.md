432e81c4369aa703dd2037ea5fb3b61d222ac262
Guy Guyon <guyon.guy@sfr.fr>
2021-10-15 22:58:07 +0200

    The project is completed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# couleur-de-fond-dynamique
+# Couleur de fond dynamique
+
+Le but est de laisser l'utilisateur changer à loisir la couleur de fond de la page
+
+## On doit pouvoir
+
+1. Lorsqu'on clic sur une des couleurs de la liste, le _background_ de la page doit adopter cette couleur.
+2. L'utilisateur doit pouvoir ajouter des couleurs à la liste, en utilisant un formulaire.
+3. Quand une couleur est ajoutée à la liste, le _background_ de la page web doit changer.
+4. Si l'utilisateur propose un nom de couleur invalide, il doit être ignoré.
+5. Vider le champ texte après l'ajout d'une couleur valide.
---
+++ js/app.js
@@ -0,0 +1,66 @@
+
+let app = {
+
+  colors: [
+    'lightsteelblue',
+    'palegreen',
+    'crimson',
+    "antiquewhite",
+    "coral",
+    "aquamarine",
+    "darkgray",
+    "purple",
+    "blue"
+  ],
+
+  content: document.querySelector('.content'),
+
+  init: function() {
+    app.generateForm();
+    for (index in app.colors) {
+      app.generateColorBox(app.colors[index]);
+    }
+  },
+
+  generateForm: function() {
+    let form = document.createElement('form');
+    form.classList.add('bg-color-form');
+    form.addEventListener('submit', app.handleColorAdd);
+
+    let input = document.createElement('input');
+    input.classList.add('bg-color-form-input');
+    input.type = 'text';
+    input.placeholder = 'Couleur CSS';
+
+    form.appendChild(input);
+    app.content.appendChild(form);
+  },
+
+  generateColorBox: function(color) {
+    let colorBox = document.createElement('div');
+    colorBox.className = 'bg-color-selector';
+    colorBox.style.background = color;
+    colorBox.innerText = color;
+    colorBox.addEventListener('click', app.handleColorClick);
+    app.content.appendChild(colorBox);
+    return colorBox;
+  },
+
+  handleColorAdd: function(event) {
+    event.preventDefault();
+    let input = event.target.querySelector('.bg-color-form-input');
+    // let input = document.querySelector('.bg-color-form-input');
+    let color = input.value;
+    if (!cssColors.includes(color)) return;
+    let box = app.generateColorBox(color);
+    input.value = '';
+    box.click();
+  },
+
+  handleColorClick: function(event) {
+    let color = event.target.style.background;
+    document.body.style.background = color;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
